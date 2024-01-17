d6038e11beceeab241338880fcccd6d6e5ae7e13
LetsGoMan <ilikeseik@gmail.com>
2024-01-17 17:59:04 +0300

    fix file structure, adding a server to run using a script, add readme

--- utils.js
+++
@@ -1,8 +0,0 @@
-export function getResult(expression) {
-  try {
-      return Function(`'use strict'; return (${expression});`)();
-  } catch (error) {
-    console.error('Error evaluating expression:', error);
-    return expression;
-  }
-}
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './scripts/script.js', // Путь к главному файлу вашего приложения
+  entry: './src/scripts/script.js', // Путь к главному файлу вашего приложения
   output: {
     filename: 'bundle.js', // Имя выходного файла JavaScript
     path: path.resolve(__dirname, 'dist'), // Путь к папке выходного файла
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html', // Путь к шаблону HTML
+      template: './src/index.html', // Путь к шаблону HTML
       filename: 'index.html', // Имя выходного файла HTML
     }),
+
   ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // применять правила только к CSS-файлам
+        use: ['style-loader', 'css-loader'] // загрузчики для обработки CSS
+      }
+    ]
+  }
 };
